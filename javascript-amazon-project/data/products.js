feb612a00dafb08d81ab176d9e304eeffef31efb
atharva shrestha <atharvakop7@gmail.com>
2026-02-09 13:25:47 +0545

    learned more details about classses and this

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -58,24 +58,22 @@ class Clothing extends Product {
 
 }
 
-const tshirt = new Clothing({
-    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799,
-    keywords: [
-        "tshirts",
-        "apparel",
-        "mens"
-    ],
-    type: "clothing",
-    sizeChartLink: "images/clothing-size-chart.png"
-});
-console.log(tshirt)
+// const date = new date()
+// console.log(date)
+// console.log(date.toLocaleTimeString())
+// function logThis() {
+//     console.log(this)
+// }
+//
+// logThis.call('hello')
+//
+// this
+// const object = {
+//     method : ()=>{
+//         console.log(this)
+//     }
+// }
+// object.method()
 
 export const products = [
     {
